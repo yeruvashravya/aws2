266c0123a32a8142eda3ae4e40bc94e16d7e14e3
Yeruva Shravya <shravyayeruva012@gmail.com>
2025-07-04 11:40:06 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentTable from './StudentTable';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Student Records</h1>
+      <StudentTable />
+    </div>
+  );
+}
+
+export default App;
